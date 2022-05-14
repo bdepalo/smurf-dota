dcb1172914cd9cec712d0abdf3f2d48490a6684e
Brandon DePalo <brandon.depalo@gmail.com>
2022-05-14 13:32:40 -0400

    fixed cors import

--- rest-api/src/main/java/org/threetwotwo/rest/SmurfRestController.java
+++ rest-api/src/main/java/org/threetwotwo/rest/SmurfRestController.java
@@ -1,5 +1,6 @@
 package org.threetwotwo.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
