99db8f4dd692fbbf06fcc3a537c9d8a9eb366151
Brandon DePalo <brandon.depalo@gmail.com>
2022-07-30 23:38:46 -0400

    changed urls for https / swag

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,9 +6,9 @@ ReactDOM.render(
     <div>
         <h1>Hello, Welcome to 322</h1>
         <hr/>
-        <BuildButtonComponent header="Full Random" endpoint="http://rollterps.duckdns.org:32280/random" text="Click Me"/>
-        <BuildButtonComponent header="No Consumables" endpoint="http://rollterps.duckdns.org:32280/random/no-consumables" text="Click Me"/>
-        <BuildButtonComponent header="Normal Random" endpoint="http://rollterps.duckdns.org:32280/random/normal" text="Click Me"/>
+        <BuildButtonComponent header="Full Random" endpoint="https://rollterps.duckdns.org/smurf/api/random" text="Click Me"/>
+        <BuildButtonComponent header="No Consumables" endpoint="https://rollterps.duckdns.org/smurf/api/random/no-consumables" text="Click Me"/>
+        <BuildButtonComponent header="Normal Random" endpoint="https://rollterps.duckdns.org/smurf/api/random/normal" text="Click Me"/>
     </div>,
     document.getElementById("root")
 );
