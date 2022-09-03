06fc97e9508eb316ff6c893fb3b360983548fd64
Brandon DePalo <brandon.depalo@gmail.com>
2022-09-03 13:40:29 -0400

    adjust itemcap endpoint

--- rest-api/src/test/java/org/threetwotwo/rest/TestSmurfRestController.java
+++ rest-api/src/test/java/org/threetwotwo/rest/TestSmurfRestController.java
@@ -1,14 +1,18 @@
 package org.threetwotwo.rest;
 
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 public class TestSmurfRestController {
 
     @Test
-    public void gitDotGud(){
+    @Disabled
+    public void testNoClarity(){
         SmurfRestController controller = new SmurfRestController();
-        System.out.println(controller.getRandomStartingBuildWithItemCaps(Map.of("clarity", 0)));
+        System.out.println(controller.getRandomStartingBuildWithItemCaps(Map.of("clarity", List.of(0,0))));
     }
 }
