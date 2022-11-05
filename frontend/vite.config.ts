c2e461bfd383808063e6868dc5881661e4d49e3c
Brandon DePalo <brandon.depalo@gmail.com>
2022-11-05 14:53:42 -0400

    fix(frontend): add some routes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/smurf/",
   plugins: [vue()],
   server: {
     port: 30322,
     host: "0.0.0.0"
-  }
+  },
 })
