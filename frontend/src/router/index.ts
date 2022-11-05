ab503794f35f19a87bda75e59afa37576e7e117f
Brandon DePalo <brandon.depalo@gmail.com>
2022-11-05 14:58:53 -0400

    fix(frontend): temporarily redirect traffic to home

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,15 +3,15 @@ import Home from "../views/Home.vue";
 import About from "../views/About.vue"
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
+    path: "/*",
     name: "Home",
     component: Home
   },
-  {
-    path: "/*",
-    name: "About",
-    component: About
-  }
+  // {
+  //   path: "/*",
+  //   name: "About",
+  //   component: About
+  // }
 ]
 const router = createRouter({
   history: createWebHistory(),
